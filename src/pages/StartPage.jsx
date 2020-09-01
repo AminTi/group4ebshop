87eaa2750cc07ca66ece2d94a9f01080df65698b
AminTi <titiamin@icloud>
2020-09-01 17:07:17 +0200

    Amin

---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import FetchData from "../components/FetchData"
+
+function StartPage() {
+    return (
+        <>
+            <FetchData />
+        </>
+    )
+}
+
+export default StartPage
