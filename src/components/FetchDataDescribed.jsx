0c2b333ec6f1b7058aa7999f8c470d5f44e15e56
AminTi <titiamin@icloud>
2020-09-01 22:00:40 +0200

    test

--- src/components/FetchDataDescribed.jsx
+++ src/components/FetchDataDescribed.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import ProductDetailed from "./ProductDetailed";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import ProductDetailed from "./ProductDetailed"
 // import ProductList from "./ProductList";
 // import ProductDetailed from "./ProductDetailed";
 
 const FetchDataDescribed = ({ productId }) => {
-  const [data, setData] = useState({});
+    const [data, setData] = useState({})
 
-  const DataHandler = () => {
-    axios
-      .get(
-        `https://mock-data-api.firebaseio.com/e-commerce/products/${productId}.json`
-      )
-      .then((res) => {
-        return setData(res.data);
-      });
-  };
+    const DataHandler = () => {
+        axios
+            .get(
+                `https://mock-data-api.firebaseio.com/e-commerce/products/${productId}.json`
+            )
+            .then((res) => {
+                return setData(res.data)
+            })
+    }
 
-  useEffect(() => {
-    DataHandler();
-  }, []);
+    useEffect(() => {
+        DataHandler()
+    }, [])
 
-  return (
-    <>
-      <ProductDetailed data={data} />
-    </>
-  );
-};
+    return (
+        <>
+            <ProductDetailed data={data} />
+        </>
+    )
+}
 
-export default FetchDataDescribed;
+export default FetchDataDescribed
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useRef } from "react"
 import { BuyButton } from "./BuyButton"
 
@@ -17,45 +16,8 @@ const ProductList = ({ name, description, price, rating, stock, images }) => {
             paragraf.current.innerText = ""
             setShowHide(true)
         }
-=======
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
-const ProductList = ({
-  key,
-  id,
-  name,
-  description,
-  price,
-  rating,
-  stock,
-  images,
-}) => {
-  const [showHide, setShowHide] = useState(false);
-
-  const display = () => {
-    if (showHide) {
-      return (
-        <div>
-          {description}
-          {rating}
-        </div>
-      );
-    } else {
-      return "";
     }
-  };
 
-  const hide = () => {
-    if (showHide === false) {
-      return <button onClick={() => setShowHide(true)}>Read More</button>;
-    } else {
-      return <button onClick={() => setShowHide(false)}>Hide details</button>;
->>>>>>> d233559ed13ec62bb32b9be5bc6486d2d387bb77
-    }
-  };
-
-<<<<<<< HEAD
     return (
         <div className="items-wrapper">
             <div className="Image-Container">
@@ -79,22 +41,19 @@ const ProductList = ({
         </div>
     )
 }
-=======
-  return (
-    <div className="items-wrapper">
-      <div className="Image-Container">
-        <img src={images[0].src.small} alt={images[0].alt} className="Image" />
-      </div>
+//   return (
+//     <div className="items-wrapper">
+//       <div className="Image-Container">
+//         <img src={images[0].src.small} alt={images[0].alt} className="Image" />
+//       </div>
 
-      <span>{name}</span>
-      <span>{price}</span>
-      <span>{stock}</span>
-      {display()}
-      {hide()}
-      <Link to={`/product/${id}`} id={id}>Detailed Page</Link>
-    </div>
-  );
-};
->>>>>>> d233559ed13ec62bb32b9be5bc6486d2d387bb77
+//       <span>{name}</span>
+//       <span>{price}</span>
+//       <span>{stock}</span>
+//       {display()}
+//       {hide()}
+//       <Link to={`/product/${id}`} id={id}>Detailed Page</Link>
+//     </div>
+//   );
 
-export default ProductList;
+export default ProductList
