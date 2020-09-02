23ded4f98611a74f77e19d67a493a3f6d9c5a024
AminTi <titiamin@icloud>
2020-09-02 10:42:14 +0200

    Evelina

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from "react"
+import { CartContext } from "../context/CartContext"
 
 function CheckoutPage() {
-  return (
-    <div>
-      <h3>CheckoutPage</h3>
-    </div>
-  );
+    return (
+        <div>
+            <h3>CheckoutPage</h3>
+        </div>
+    )
 }
 
-export default CheckoutPage;
+export default CheckoutPage
