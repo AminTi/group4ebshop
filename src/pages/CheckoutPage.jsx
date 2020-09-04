1848b48d2d5f03392db5a8e9a0a59b8978a715b4
AminTi <titiamin@icloud>
2020-09-04 10:47:54 +0200

    Nan a number fix

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,113 +1,107 @@
-import React, { useContext, useRef, useState, useEffect } from "react";
-import axios from "axios";
+import React, { useContext, useRef, useState, useEffect } from "react"
+import axios from "axios"
 
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../context/CartContext"
 
-import TableStartPage from "../components/TableStartPage";
-import Order from "../components/Order";
+import TableStartPage from "../components/TableStartPage"
+import Order from "../components/Order"
 
 function CheckoutPage() {
-  const context = useContext(CartContext);
-  const [discountCupon, setDescount] = useState({});
-  const [inputValue, setinputValue] = useState();
-  const [discountPrice, setDiscountPrice] = useState([]);
-  const apiKey = useRef();
+    const context = useContext(CartContext)
+    const [discountCupon, setDescount] = useState({})
+    const [inputValue, setinputValue] = useState()
+    const [discountPrice, setDiscountPrice] = useState([])
+    const apiKey = useRef()
+    let productTotalPrice = parseInt(context.totalSum)
 
-  const discountValues = (e) => {
-    setinputValue(apiKey.current.value.toUpperCase());
-    const descountApi = `https://mock-data-api.firebaseio.com/e-commerce/couponCodes/${inputValue}.json`;
-    axios.get(descountApi).then((res) => {
-      setDescount(res.data);
-    });
-  };
-
-  console.log(inputValue);
-  console.log(discountCupon);
-  console.log(discountPrice);
-
-  useEffect(() => {
-    discountValues();
-  }, [inputValue]);
-
-  useEffect(() => {
-    setDiscountPrice(discountCounter());
-  }, [discountCupon]);
-
-  const discountCounter = () => {
-    let productTotalPrice = parseInt(context.totalSum);
-    const discountprice = discountCupon && discountCupon.discount;
-    if (
-      inputValue === "BLACKFRIDAY" ||
-      inputValue === "SUMMER19" ||
-      inputValue === "BLACKFRIDAY2019"
-    ) {
-      const disTotal = productTotalPrice * discountprice;
-      return disTotal;
-    } else {
-      const finalPrice = productTotalPrice;
-      return finalPrice;
+    const discountValues = (e) => {
+        setinputValue(apiKey.current.value.toUpperCase())
+        const descountApi = `https://mock-data-api.firebaseio.com/e-commerce/couponCodes/${inputValue}.json`
+        axios.get(descountApi).then((res) => {
+            setDescount(res.data)
+        })
     }
-  };
 
-  const ErrorCheck = () => {
-    if (
-      inputValue === "BLACKFRIDAY" ||
-      inputValue === "SUMMER19" ||
-      inputValue === "BLACKFRIDAY2019"
-    ) {
-      return (apiKey.current.disabled = true);
+    useEffect(() => {
+        discountValues()
+    }, [inputValue])
+
+    useEffect(() => {
+        setDiscountPrice(discountCounter())
+    }, [discountCupon])
+
+    const discountCounter = () => {
+        const discountprice = discountCupon && discountCupon.discount
+        if (
+            inputValue === "BLACKFRIDAY" ||
+            inputValue === "SUMMER19" ||
+            inputValue === "BLACKFRIDAY2019"
+        ) {
+            const disTotal = productTotalPrice * discountprice
+            return disTotal
+        } else {
+            const finalPrice = productTotalPrice
+            return finalPrice
+        }
     }
-  };
 
-  return (
-    <div className="checkout-container">
-      <h3 className="checkout-header">Welcome to Checkout</h3>
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col"> Product</th>
-            <th scope="col"> Quantity </th>
-            <th scope="col">Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {context.cartItems.map((item, index) => {
-            return (
-              <TableStartPage
-                key={index}
-                img={item.image}
-                name={item.name}
-                quantity={item.quantity}
-                price={item.price}
-              />
-            );
-          })}
-        </tbody>
-        <tfoot></tfoot>
-      </table>
+    const ErrorCheck = () => {
+        if (
+            inputValue === "BLACKFRIDAY" ||
+            inputValue === "SUMMER19" ||
+            inputValue === "BLACKFRIDAY2019"
+        ) {
+            return (apiKey.current.disabled = true)
+        }
+    }
 
-      <input type="text" ref={apiKey} />
-      {ErrorCheck() && <span> Discount applied </span>}
+    return (
+        <div className="checkout-container">
+            <h3 className="checkout-header">Welcome to Checkout</h3>
+            <table className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col"> Product</th>
+                        <th scope="col"> Quantity </th>
+                        <th scope="col">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {context.cartItems.map((item, index) => {
+                        return (
+                            <TableStartPage
+                                key={index}
+                                img={item.image}
+                                name={item.name}
+                                quantity={item.quantity}
+                                price={item.price}
+                            />
+                        )
+                    })}
+                </tbody>
+                <tfoot></tfoot>
+            </table>
 
-      {discountCupon == null && <span> Invalid Coupon code</span>}
-      <button
-        className="button-discount btn btn-primary btn-sm"
-        onClick={discountValues}
-      >
-        Apply discount
-      </button>
-      <button
-        className="button-discount btn btn-success btn-sm"
-        onClick={discountValues}
-      >
-        Order
-      </button>
-      <h5 className="checkout-price">
-        Total Price: {Math.ceil(discountPrice)}:-
-      </h5>
-    </div>
-  );
+            <input type="text" ref={apiKey} />
+            {ErrorCheck() && <span> Discount applied </span>}
+
+            {discountCupon == null && <span> Invalid Coupon code</span>}
+            <button
+                className="button-discount btn btn-primary btn-sm"
+                onClick={discountValues}>
+                Apply discount
+            </button>
+            <button
+                className="button-discount btn btn-success btn-sm"
+                onClick={discountValues}>
+                Order
+            </button>
+            <h5 className="checkout-price">
+                Total Price: {Math.ceil(discountPrice)}:-
+            </h5>
+        </div>
+    )
 }
 
-export default CheckoutPage;
+export default CheckoutPage
